e57548f30bc81b441710f2a9424ab73696b47db1
jtorsa <jtorsadaw@gmail.com>
2020-03-16 10:32:05 +0100

    [ADD] Evento CRUD

---
+++ src/Form/EventoType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Evento;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('descripcion')
+            ->add('direccion')
+            ->add('start')
+            ->add('requeridos')
+            ->add('deporte')
+            ->add('localidad')
+            ->add('nivel')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Evento::class,
+        ]);
+    }
+}
